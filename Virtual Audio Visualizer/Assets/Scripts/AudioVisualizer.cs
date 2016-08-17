bf829632115060912dcd65d5552e1de11aab2145
shakil14cse <shakil14cse@gmail.com>
2016-08-18 02:45:51 +0600

    BAR gENERATION dOnE

--- Virtual Audio Visualizer/Assets/Scripts/AudioVisualizer.cs
+++ Virtual Audio Visualizer/Assets/Scripts/AudioVisualizer.cs
@@ -5,11 +5,11 @@ public class AudioVisualizer : MonoBehaviour
 {
 
     float[] spectrum;
+    public FFTWindow window = FFTWindow.Rectangular;
     public int spectrumSize = 256;
     public GameObject soundBarsParent;
     public GameObject[] soundBars;
     private int totalDividationBars = 10;
-    private float sumOfNeighbours = 0;
 
     void Start()
     {
@@ -31,29 +31,13 @@ public class AudioVisualizer : MonoBehaviour
     {
         if (totalDividationBars > 0)
         {
-            AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Blackman);
+            AudioListener.GetSpectrumData(spectrum, 0, window);
 
-            for (int i = 0; i < spectrum.Length - (spectrum.Length / totalDividationBars); i += (spectrum.Length / totalDividationBars))
+            for (int i = 0; i < totalDividationBars; i ++)
             {
-                sumOfNeighbours = 0.0f;
-                for (int j = i; j < i + (spectrum.Length / totalDividationBars); j++)
-                {
-                    sumOfNeighbours += spectrum[j] * 10.0f;
-                }
-                Debug.LogWarning((i / (spectrum.Length / totalDividationBars)) + ": " + sumOfNeighbours * 10.0f);
-                soundBars[(i / (spectrum.Length / totalDividationBars))].transform.localScale =
-                    new Vector3(1.0f, sumOfNeighbours > 1.0f ? sumOfNeighbours : 1.0f, 1.0f);
-            }
-            if ((spectrum.Length - (spectrum.Length / totalDividationBars)) > 0)
-            {
-                sumOfNeighbours = 0.0f;
-                for (int i = (spectrum.Length - (spectrum.Length / totalDividationBars)); i < spectrum.Length; i++)
-                {
-                    sumOfNeighbours += spectrum[i] * 10.0f;
-                }
-                soundBars[totalDividationBars - 1].transform.localScale =
-                    new Vector3(1.0f, sumOfNeighbours > 1.0f ? sumOfNeighbours : 1.0f, 1.0f);
-                Debug.LogWarning(totalDividationBars + ": " + sumOfNeighbours * 10.0f);
+                Debug.LogWarning(i + ": " + spectrum[i] * 10.0f);
+                soundBars[i].transform.localScale =
+                    new Vector3(1.0f, spectrum[i] * 10.0f > 1.0f ? spectrum[i] * 10.0f : 1.0f, 1.0f);
             }
         }
     }
