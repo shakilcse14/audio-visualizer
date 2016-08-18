bc11bd71a21f16343bc993bf7e8776209d91ef70
Md.Mustafizur Rahman Shakil <mustafizur.rahman@infolytx.com>
2016-08-18 13:32:48 +0600

    Editor Customization Added

---
+++ Virtual Audio Visualizer/Assets/Editor/VisualizerEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(AudioVisualizer))]
+public class VisualizerEditor : Editor {
+
+	public override void OnInspectorGUI ()
+	{
+		var visualizer = target as AudioVisualizer;
+
+		if (visualizer.mode == AudioVisualizer.Mode.Auto) {
+			visualizer.divideBarCount = EditorGUILayout.IntSlider ("Total Bar", visualizer.divideBarCount,
+				1, visualizer.spectrumSize);
+			visualizer.type = (AudioVisualizer.CreationType)EditorGUILayout.EnumPopup ("Creation Type", visualizer.type);
+			if (visualizer.type == AudioVisualizer.CreationType.Primitive) {
+				visualizer.primitiveType = (PrimitiveType)EditorGUILayout.EnumPopup ("Primitive Type", visualizer.primitiveType);
+			}
+			else if (visualizer.type == AudioVisualizer.CreationType.Prefab) {
+				visualizer.barPrefab = (GameObject) EditorGUILayout.ObjectField("Bar Prefab", visualizer.barPrefab,
+					typeof (GameObject), true);
+			}
+		}
+		else if (visualizer.mode == AudioVisualizer.Mode.Manual) {
+			visualizer.soundBarsParent = (GameObject)EditorGUILayout.ObjectField ("Bars Parent", visualizer.soundBarsParent,
+				typeof (GameObject), true);
+		}
+
+		base.OnInspectorGUI ();
+	}
+}
--- Virtual Audio Visualizer/Assets/Scripts/AudioVisualizer.cs
+++ Virtual Audio Visualizer/Assets/Scripts/AudioVisualizer.cs
@@ -3,42 +3,86 @@ using System.Collections;
 
 public class AudioVisualizer : MonoBehaviour
 {
-
+	public enum Mode
+	{
+		Auto,
+		Manual
+	};
+	public Mode mode = Mode.Auto;
+	public enum CreationType{
+		Primitive,
+		Prefab
+	};
+	[HideInInspector]
+	public CreationType type = CreationType.Primitive;
+	[HideInInspector]
+	public PrimitiveType primitiveType = PrimitiveType.Cube;
     float[] spectrum;
     public FFTWindow window = FFTWindow.Rectangular;
     public int spectrumSize = 256;
+	[HideInInspector]
     public GameObject soundBarsParent;
     public GameObject[] soundBars;
+	[HideInInspector]
+	public int divideBarCount = 10;
+	[HideInInspector]
+	public GameObject barPrefab;
     private int totalDividationBars = 10;
 
     void Start()
     {
         spectrum = new float[spectrumSize];
-        int i = 0;
-        soundBars = new GameObject[soundBarsParent.transform.childCount];
-        if (soundBarsParent != null)
-        {
-            foreach (Transform gme in soundBarsParent.transform)
-            {
-                soundBars[i] = gme.gameObject;
-                i++;
-            }
-        }
-        totalDividationBars = soundBars.Length;
-    }
+        int count = 0;
+		if (mode == Mode.Manual) {
+			soundBars = new GameObject[soundBarsParent.transform.childCount];
+			if (soundBarsParent != null) {
+				foreach (Transform gme in soundBarsParent.transform) {
+					soundBars [count] = gme.gameObject;
+					count++;
+				}
+			}
+		} else if (mode == Mode.Auto) {
+			soundBars = new GameObject[divideBarCount];
+			for (int i = 0; i < divideBarCount; i++) {
+				if (type == CreationType.Primitive) {
+					soundBars [i] = GameObject.CreatePrimitive (primitiveType);
+					DestroyImmediate (soundBars [i].GetComponent<Collider> ());
+				}
+				else if (type == CreationType.Prefab) {
+					if (barPrefab != null) {
+						soundBars [i] = (GameObject)Instantiate (barPrefab, Vector3.zero, Quaternion.identity);
+					}
+					else {
+						soundBars = null;
+						return;
+					}
+				}
+				if (soundBars [i] != null) {
+					soundBars [i].transform.parent = transform;
+					soundBars [i].transform.localScale = Vector3.one;
+					soundBars [i].transform.localPosition = new Vector3 (i - divideBarCount / 2, 0.0f, 0.0f);
+				}
+			}
+		}
+		if (soundBars != null) {
+			totalDividationBars = soundBars.Length;
+		}
+	}
 
     void Update()
     {
         if (totalDividationBars > 0)
-        {
-            AudioListener.GetSpectrumData(spectrum, 0, window);
+		{
+			if (soundBars != null) {
+				
+				AudioListener.GetSpectrumData (spectrum, 0, window);
 
-            for (int i = 0; i < totalDividationBars; i ++)
-            {
-                Debug.LogWarning(i + ": " + spectrum[i] * 10.0f);
-                soundBars[i].transform.localScale =
-                    new Vector3(1.0f, spectrum[i] * 10.0f > 1.0f ? spectrum[i] * 10.0f : 1.0f, 1.0f);
-            }
+				for (int i = 0; i < totalDividationBars; i++) {
+					Debug.LogWarning (i + ": " + spectrum [i] * 10.0f);
+					soundBars [i].transform.localScale =
+                    new Vector3 (1.0f, spectrum [i] * 10.0f > 1.0f ? spectrum [i] * 10.0f : 1.0f, 1.0f);
+				}
+			}
         }
     }
 }
